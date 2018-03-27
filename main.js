9576621d7dc0a810bc8f5fad4afec7ba98df9b7c
mehtapaydin <mehtapaydinnnn@gmail.com>
2018-03-27 13:53:35 -0400

    ..

---
+++ main.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const scene = document.querySelector('a-scene');
+    const splash = document.querySelector('#splash');
+    const loading = document.querySelector('#splash .loading');
+    const startButton = document.querySelector('#splash .start-button');
+
+    const creditsToggle = document.querySelector('#splash .show-credits');
+    const creditsList = document.querySelector('#splash .credits-list');
+
+    const emitEvent = (eventName, listeners) => {
+        listeners.forEach((listener) => {
+            const el = document.querySelector(listener);
+            el.emit(eventName);
+        })
+    };
+
+    const emitMediaEvent = (eventType, listeners) => {
+        listeners.forEach((listener) => {
+            const el = document.querySelector(listener);
+            el.components.sound[eventType]();
+        })
+    };
+
+    const activateSoundsForTouch = () => {
+    	const sounds = document.querySelectorAll('a-sound')
+        sounds.forEach((soundEl) => {
+            soundEl.components.sound.playSound();
+            soundEl.components.sound.stopSound();
+        })
+    };
+
+    scene.addEventListener('loaded', function (e) {
+        setTimeout(() => {
+            loading.style.display = 'none';
+            splash.style.backgroundColor = 'rgba(0, 0, 0, 0.85)';
+            startButton.style.opacity = 1;
+            creditsToggle.style.opacity = 1;
+        }, 50);
+    });
+
+    creditsToggle.addEventListener('click', function (e) {
+    	e.preventDefault();
+
+    	var display = creditsList.style.display === 'none'
+            || creditsList.style.display === '' ? 'block' : 'none';
+        
+        creditsList.style.display = display;
+
+        return false;
+    });
+
+    startButton.addEventListener('click', function (e) {
+        activateSoundsForTouch();
+        splash.style.display = 'none';
+        emitEvent('scene-started', ['#planet', '#shuttle-light', '#shuttle-light', '#shuttle', '#shuttle']);
+    });
+});
